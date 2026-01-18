f09b432383abe401be2d06dce6fc6d58b4eea9b6
Denwer-DE <mefestofel87@gmail.com>
2026-01-18 09:38:14 +0100

    Add files via upload

--- remove_ads.js
+++ remove_ads.js
@@ -1,30 +1,19 @@
 (function () {
-  'use strict';
+    function initLampaHook() {
+        if (window.Lampa && Lampa.Player && Lampa.Player.play) {
+            var originalPlay = Lampa.Player.play;
+            Lampa.Player.play = function (object) {
+                object.iptv = true;
 
-  var config = {
-    version: '1.0.0',
-    name: 'Remove Ads'
-  };
-
-  function init() {
-    if (window.Lampa && Lampa.Player && Lampa.Player.play) {
-      var originalPlay = Lampa.Player.play;
-      Lampa.Player.play = function (object) {
-        object.iptv = true;
-
-        if (object.vast_url) delete object.vast_url;
-        if (object.vast_msg) delete object.vast_msg;
-
-        console.log('Remove Ads', 'Ad URLs removed from player object');
-        return originalPlay.apply(this, arguments);
-      };
-      console.log('Remove Ads', 'Configuration plugin loaded, version:', config.version);
-      console.log('Remove Ads', 'Player hook initialized successfully');
-    } else {
-      setTimeout(init, 500);
+                if (object.vast_url) delete object.vast_url;
+                if (object.vast_msg) delete object.vast_msg;
+                
+                return originalPlay.apply(this, arguments);
+            };
+        } else {
+            setTimeout(initLampaHook, 500);
+        }
     }
-  }
 
-  init();
-
+    initLampaHook();
 })();
