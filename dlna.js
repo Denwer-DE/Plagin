5e5bc5a1f2ef26d7fbbeac4e987817491f89ade4
Denwer-DE <mefestofel87@gmail.com>
2026-02-08 15:51:20 +0100

    Add files via upload

--- dlna.js
+++ dlna.js
@@ -9,81 +9,68 @@
         Lampa.Lang.add({
             client_dlna_name: { ru: 'DLNA Браузер', en: 'DLNA Browser' },
             client_dlna_empty: { ru: 'Устройства не найдены', en: 'No devices found' },
-            client_dlna_help: { ru: 'Поиск локальных медиа-серверов в вашей сети', en: 'Searching for local media servers' }
         });
 
-        // 2. Создаем внутренний компонент вместо внешнего component.js
-        function DLNAComponent(object) {
+        // 2. Правильная структура компонента для Lampa
+        Lampa.Component.add('client_dnla', function (object) {
             var network = new Lampa.Reguest();
             var scroll = new Lampa.Scroll({mask: true, over: true});
             var items = [];
             var html = $('<div></div>');
             var body = $('<div class="category-full"></div>');
             
-            this.create = function () {
+            // Этот метод Lampa вызывает автоматически при старте
+            this.start = function () {
                 var _this = this;
 
-                // Обработка кнопки "Назад"
-                this.activity.loader(true);
-                
-                // Рендерим пустой список или эмуляцию
-                this.build();
-
-                return this.render();
-            };
-
-            this.build = function () {
-                var _this = this;
+                // Убираем лоадер
                 this.activity.loader(false);
 
-                // Если мы на Tizen, здесь должна быть логика поиска устройств
-                // На других ОС просто показываем сообщение, что поиск завершен
+                // Сообщаем, что здесь пусто (так как мы отвязались от поиска Tizen)
                 var empty = $('<div class="empty">' + Lampa.Lang.translate('client_dlna_empty') + '</div>');
                 body.append(empty);
                 
                 scroll.append(body);
                 html.append(scroll.render());
+
+                // Даем знать Lampa, что компонент готов к отображению
+                if (this.onReady) this.onReady(html);
             };
 
+            // Обязательный метод для отображения содержимого
             this.render = function () {
                 return html;
             };
 
             this.pause = function () {};
             this.stop = function () {};
+            
+            // Очистка памяти при закрытии
             this.destroy = function () {
                 network.clear();
                 scroll.destroy();
-                html.remove();
+                if (html) html.remove();
                 items = null;
             };
-        }
+        });
 
-        // 3. Регистрация компонента в системе Lampa
-        Lampa.Component.add('client_dnla', DLNAComponent);
-
-        // 4. Добавление кнопки в меню
+        // 3. Добавление кнопки в меню
         function addMenuButton() {
-            var manifest = {
-                name: 'DLNA',
-                component: 'client_dnla',
-            };
-
             var button = $(`<li class="menu__item selector">
                 <div class="menu__ico">
                     <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
-                        <circle cx="256" cy="256" r="200" stroke="currentColor" stroke-width="30" fill="none" />
-                        <circle cx="256" cy="256" r="60" fill="currentColor" />
+                        <path fill="currentColor" d="M256 0C114.8 0 0 114.8 0 256s114.8 256 256 256 256-114.8 256-256S397.2 0 256 0zm0 472c-119.3 0-216-96.7-216-216S136.7 40 256 40s216 96.7 216 216-96.7 216-216 216z"/>
+                        <circle cx="256" cy="256" r="80" fill="currentColor" />
                     </svg>
                 </div>
-                <div class="menu__text">${manifest.name}</div>
+                <div class="menu__text">DLNA</div>
             </li>`);
 
             button.on('hover:enter', function () {
                 Lampa.Activity.push({
                     url: '',
-                    title: manifest.name,
-                    component: manifest.component,
+                    title: 'DLNA',
+                    component: 'client_dnla',
                     page: 1
                 });
             });
@@ -91,7 +78,6 @@
             $('.menu .menu__list').eq(0).append(button);
         }
 
-        // Запуск при готовности приложения
         if (window.appready) addMenuButton();
         else {
             Lampa.Listener.follow('app', function (e) {
@@ -100,8 +86,5 @@
         }
     }
 
-    // Инициализация
-    if (typeof Lampa !== 'undefined') {
-        startPlugin();
-    }
+    if (typeof Lampa !== 'undefined') startPlugin();
 })();
